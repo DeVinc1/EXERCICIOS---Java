1ad374b57706a899ea7d7ecc27198bc8e85554db
Marcos Vinícius <101370923+DeVinc1@users.noreply.github.com>
2023-02-23 10:07:44 -0300

    Refatorando código da classe de calculadora [FINALIZADO]

--- exercicio2/calculadora.java
+++ exercicio2/calculadora.java
@@ -15,12 +15,17 @@ public class calculadora {
     }
 
     public double dividir(double x, double y){
+        /*
+         * Tratamento de erro de divisão por 0
+         */
         try {
+            
             if(y == 0){
                 throw new ArithmeticException("A divisão por zero não é permitida. \n");
             }else{
                 return x / y;
             }
+            
         } catch (ArithmeticException e) {
             System.out.println("Ocorreu um erro: " + e.getMessage());
             return Double.NaN; 
