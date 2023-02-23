0190e35b91ba4d9519f0bf06b7579973ca54f018
Marcos Vinícius <101370923+DeVinc1@users.noreply.github.com>
2023-02-23 10:04:36 -0300

    Adicionando o exercício 2 ao repositório

---
+++ exercicio2/calculadora.java
@@ -0,0 +1,30 @@
+package exercicio2;
+
+public class calculadora {
+    
+    public double somar(double x, double y){
+        return x + y;
+    }
+
+    public double subtrair(double x, double y){
+        return x - y;
+    }
+
+    public double multiplicar(double x, double y){
+        return x * y;
+    }
+
+    public double dividir(double x, double y){
+        try {
+            if(y == 0){
+                throw new ArithmeticException("A divisão por zero não é permitida. \n");
+            }else{
+                return x / y;
+            }
+        } catch (ArithmeticException e) {
+            System.out.println("Ocorreu um erro: " + e.getMessage());
+            return Double.NaN; 
+        }
+
+    }
+}
---
+++ exercicio2/mainClass.java
@@ -0,0 +1,91 @@
+package exercicio2;
+
+import java.util.Scanner;
+
+public class mainClass {
+
+    public static void main(String[] args) {
+        /*
+         * Instâncias de Classes
+         */
+        Scanner input = new Scanner(System.in);
+        calculadora calculo = new calculadora();
+
+        /*
+         * Cabeçalho
+         */
+        System.out.println("---------------------------");
+        System.out.println("|       CALCULADORA       |");
+        System.out.println("---------------------------");
+
+        /*
+         * Fluxo de execução do Código
+         */
+        boolean continuar = true;
+        char operacao = 'z';
+
+        double num1 = 0;
+        double num2 = 0;
+
+        while (continuar == true) {
+
+            System.out.println( "\nEscolha uma das seguintes operações matemáticas: \n\t a. SOMAR \n\t b. SUBTRAIR \n\t c. MULTIPLICAR \n\t d. DIVIDIR \n\t z. ENCERRAR O PROGRAMA");
+            operacao = input.next().charAt(0);
+
+            switch (operacao) {
+                //Soma
+                case 'a':
+                    System.out.print("\nDigite o primeiro número: ");
+                    num1 = input.nextDouble();
+
+                    System.out.print("Digite o segundo número: ");
+                    num2 = input.nextDouble();
+
+                    System.out.println("O resultado da soma é " + calculo.somar(num1, num2));
+                    break;
+                //Subtração
+                case 'b':
+                    System.out.print("\nDigite o primeiro número: ");
+                    num1 = input.nextDouble();
+
+                    System.out.print("Digite o segundo número: ");
+                    num2 = input.nextDouble();
+
+                    System.out.println("O resultado da subtração é " + calculo.subtrair(num1, num2));
+                    break;
+                //Multiplicação
+                case 'c':
+                    System.out.print("\nDigite o primeiro número: ");
+                    num1 = input.nextDouble();
+
+                    System.out.print("Digite o segundo número: ");
+                    num2 = input.nextDouble();
+
+                    System.out.println("O resultado da multiplicação é " + calculo.multiplicar(num1, num2));
+                    break;
+                //Divisão
+                case 'd':
+                    System.out.print("\nDigite o primeiro número: ");
+                    num1 = input.nextDouble();
+
+                    System.out.print("Digite o segundo número: ");
+                    num2 = input.nextDouble();
+
+                    System.out.println("O resultado da divisão é " + calculo.dividir(num1, num2));
+                    break;
+                //Encerramento
+                case 'z':
+                    System.out.println("Encerrando o programa...");
+                    continuar = false;
+                    break;
+                //Qualquer outro valor
+                default:
+                    System.out.println("Opção inválida. Tente denovo.");
+                    break;
+            }
+        }
+
+        input.close();
+    }
+
+}
